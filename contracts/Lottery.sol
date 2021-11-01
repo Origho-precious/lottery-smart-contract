f14fefec63ec3d794a171fee5a46af36c628e4a9
Origho-precious <Origh09@gmail.com>
2021-11-01 23:17:39 +0100

    feat: adding Lottery contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+    
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable{
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+    
+    function random() private view returns(uint){
+        return uint(keccak256(abi.encode(block.difficulty, block.timestamp, players)));
+    }
+    
+    function pickWinner() public restricted{
+        uint index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function getPlayers() public view returns(address[] memory){
+        return players;
+    }
+}
