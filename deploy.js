29aa7555361bd67fcee06bf3e6397d8726a38c3f
Origho-precious <Origho9@gmail.com>
2021-11-11 18:06:51 +0100

    fix: fixing deploy script error

--- deploy.js
+++ deploy.js
@@ -1,25 +1,29 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const dotenv = require("dotenv");
+
+const { abi, evm } = require("./compile");
+
+dotenv.config();
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+	process.env.MNEMONIC,
+	process.env.INFURA_URL
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+	const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+	const contract = await new web3.eth.Contract(abi)
+		.deploy({ data: evm.bytecode.object })
+		.send({ gas: "1000000", from: accounts[0] });
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+	console.log("ABI =>", abi);
 
-  console.log('Contract deployed to', result.options.address);
-  provider.engine.stop();
+	console.log("Contract deployed to =>", contract.options.address);
+	provider.engine.stop();
 };
+
 deploy();
